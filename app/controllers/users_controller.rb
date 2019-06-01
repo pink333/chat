be767f3478c81a817a41cfe2745e7cc373be342d
pink333 <pikugift@gmail.com>
2019-06-02 02:37:56 +0900

    add_:show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-   before_action :authenticate_user!, only: [:edit, :update]
+   before_action :authenticate_user!, only: [:show, :edit, :update]
    before_action :correct_user,   only: [:edit, :update]
    
   def index
